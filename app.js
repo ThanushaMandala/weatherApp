0f030bde21c744e08066e18268d6d7fbd088f495
soumytiwari <tiwarisoumya111@gmail.com>
2023-10-28 18:13:40 +0530

    toolTip fixed

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ const detailsDiv = document.querySelector("#details");
 const loadingSpinner = document.querySelector("#loadingSpinner");
 const weatherTitle = document.querySelector("#weatherTitle");
 
+// Add event listener to search form
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const city = input.value;
     const apiKey = "f235c5e839c641aa861661c219ae7237";
     const url = `https://api.weatherbit.io/v2.0/current?city=${city}&key=${apiKey}`;
 
+    // show the loading spinner while fetching data
     loadingSpinner.style.display = 'block';
 
+    // Fetch weather data for selected location
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
@@ -22,11 +25,19 @@ form.addEventListener("submit", (e) => {
             const pressure = data.data[0].pres;
 
             let iconCode = data.data[0].weather.icon;
+            // remove the first character from the icon code
+            // to get the correct icon name
             iconCode = iconCode.substring(1);
 
+            // hide the loading spinner after data is fetched
             loadingSpinner.style.display = 'none';
 
+            // Display weather data and icon
             iconDiv.innerHTML = `<i class="fas fa-${getIcon(iconCode)}"></i>`;
+
+            // set the title attribute for the tooltip
+            iconDiv.title = `Weather Condition: ${description}`;
+
             detailsDiv.innerHTML = `
                 <table>
                     <tr>
@@ -52,10 +63,12 @@ form.addEventListener("submit", (e) => {
         .catch((error) => {
             console.error(error);
             detailsDiv.innerHTML = "An error occurred while fetching weather data.";
+            // hide the loading spinner after data is fetched
             loadingSpinner.style.display = 'none';
         });
 });
 
+// Get icon code based on weather condition
 function getIcon(iconCode) {
     switch (iconCode) {
         case "01d":
